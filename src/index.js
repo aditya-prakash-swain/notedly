bcad2f9416225e46aa9cfffb0d8d43fb972b1411
aditya-prakash-swain <2002adityaprakash@gmail.com>
2024-10-04 17:42:04 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,62 @@
-// index.js
-// This is the main entry point of our application
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+require('dotenv').config();
+//const db = require('./db');
+const models = require('./models');
+const typeDefs = require('./schema');
+const resolvers = require('./resolvers');
+const jwt = require('jsonwebtoken');
+const helmet = require('helmet');
+const cors = require('cors');
+const depthLimit = require('graphql-depth-limit');
+const { createComplexityLimitRule } = require('graphql-validation-complexity');
+// add the middleware after app.use(helmet());
+const app = express();
+app.use(cors());
+// add the middleware at the top of the stack, after const app = express()
+app.use(helmet());
+
+// Run the server on a port specified in our .env file or port 4000
+const port = process.env.PORT || 4000;
+// Store the DB_HOST value as a variable
+//const DB_HOST = process.env.DB_HOST;
+
+// get the user info from a JWT
+const getUser = token => {
+  if (token) {
+    try {
+      // return the user information from the token
+      return jwt.verify(token, process.env.JWT_SECRET);
+    } catch (err) {
+      // if there's a problem with the token, throw an error
+      throw new Error('Session invalid');
+    }
+  }
+};
+
+
+// Connect to the database
+//db.connect(DB_HOST);
+// Apollo Server setup
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  validationRules: [depthLimit(5), createComplexityLimitRule(1000)],
+  context: async ({ req }) => {
+    // get the user token from the headers
+    const token = req.headers.authorization;
+    // try to retrieve a user with the token
+    const user = await getUser(token);
+    // for now, let's log the user to the console:
+    console.log(user);
+    // add the db models and the user to the context
+    return { models, user };
+  }
+});
+// Apply the Apollo GraphQL middleware and set the path to /api
+server.applyMiddleware({ app, path: '/api' });
+app.listen({ port }, () =>
+  console.log(
+    `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+  )
+);
